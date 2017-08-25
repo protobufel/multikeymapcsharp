f2da6d8080d6e8947edeedbb6f45cd5c401b6c23
protobufel <protobufel@gmail.com>
2017-08-24 18:12:49 -0700

    refactored the solution to dotnet 2.0

---
+++ MultiKeyMap.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MultiKeyMap.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
